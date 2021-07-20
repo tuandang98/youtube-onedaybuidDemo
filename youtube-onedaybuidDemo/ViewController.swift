6e9c255bed56f9822a3909b185360c407c379154
tuandang98 <watsuthevabean@gmail.com>
2021-07-20 17:37:14 +0700

    initial xcode pj

---
+++ youtube-onedaybuidDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuidDemo
+//
+//  Created by Tuấn Đặng Hoàng on 20/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
